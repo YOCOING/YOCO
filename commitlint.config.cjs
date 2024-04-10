9b706588b2149348d4626556576dcff5d4258ebd
glassk <glassk0724@gmail.com>
2024-04-10 23:07:33 +0900

    [chore] enhance commitlint rules

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,7 +1,21 @@
 module.exports = {
-  // extends: ["@commitlint/config-conventional"],
+  extends: ["@commitlint/config-conventional"],
   rules: {
-    "type-enum": [2, "always", ["chore", "docs", "feat", "fix", "refactor", "style"]],
-    "subject-case": [2, "never", ["sentence-case", "start-case", "pascal-case", "upper-case"]],
+    "type-empty": [0],
+    "subject-empty": [0],
+    "header-format-rule": [2, "always"],
   },
+  plugins: [
+    {
+      rules: {
+        "header-format-rule": ({ header }) => {
+          const commitTypeRegex = /^\[(chore|docs|feat|fix|refactor|style)\]\s.+$/;
+          return [
+            commitTypeRegex.test(header),
+            `Commit message must follow the '[type] description' format (e.g. [feat] add new feature).\nAllowed Type: chore, docs, feat, fix, refactor, style`,
+          ];
+        },
+      },
+    },
+  ],
 };
