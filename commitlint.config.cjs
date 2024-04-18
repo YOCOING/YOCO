43adc881fe50cc96dbe00854b3ea1bc196234847
givvemee <givvemeee@gmail.com>
2024-04-18 15:32:46 +0900

    [chore] added new prefix for commitlint: test

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -9,7 +9,7 @@ module.exports = {
     {
       rules: {
         "header-format-rule": ({ header }) => {
-          const commitTypeRegex = /^\[(chore|docs|feat|fix|refactor|style)\]\s.+$/;
+          const commitTypeRegex = /^\[(chore|docs|feat|fix|refactor|style|test)\]\s.+$/;
           return [
             commitTypeRegex.test(header),
             `Commit message must follow the '[type] description' format (e.g. [feat] add new feature).\nAllowed Type: chore, docs, feat, fix, refactor, style`,
