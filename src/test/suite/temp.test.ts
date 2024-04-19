11690a97b4c671e62eac137f4a8b337309bad737
givvemee <givvemeee@gmail.com>
2024-04-19 09:58:56 +0900

    [test] testing process that open files and copy & paste

--- src/test/suite/temp.test.ts
+++ src/test/suite/temp.test.ts
@@ -1,3 +1,4 @@
+import assert = require("assert");
 import { after, before } from "mocha";
 
 import * as vscode from "vscode";
@@ -8,17 +9,35 @@ suite("Temp test for main copy and paste function", () => {
   });
 
   test("Temporary test", async () => {
+    const requiredText = "// test\n";
     const documentA = await vscode.workspace.openTextDocument({
-      content: "",
+      content: "this is document A",
       language: "javascript",
     });
     const checkDocumentA = await vscode.window.showTextDocument(documentA);
 
     await checkDocumentA.edit(text => {
-      text.insert(new vscode.Position(0, 0), "// test\n");
+      text.insert(new vscode.Position(0, 0), requiredText);
     });
     await vscode.commands.executeCommand("editor.action.selectAll");
     await vscode.commands.executeCommand("editor.action.clipboardCopyAction");
+
+    const documentB = await vscode.workspace.openTextDocument({
+      content: "this is document A",
+      language: "javascript",
+    });
+    const checkDocumentB = await vscode.window.showTextDocument(documentB);
+
+    await vscode.commands.executeCommand("editor.action.clipboardPasteAction");
+
+    const expectedText = checkDocumentB.document.getText();
+    assert.strictEqual(expectedText.includes(requiredText), true, "something is wrong");
+
+    // TODO: saving files is unnecessary
+    await documentA.save();
+    await documentB.save();
+
+    vscode.window.showInformationMessage("Test is done.");
   });
 
   after(() => {
