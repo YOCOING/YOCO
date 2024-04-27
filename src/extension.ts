7ff094192e6d5df39766613d38ee31a97481bea9
Flik Jeong <jsh852@gmail.com>
2024-04-27 10:43:42 +0900

    [feat] add new command YOCO.copyTextWithFilePath

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,26 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "YOCO" is now active!');
+  // YOCO.copyTextWithFilePath
+  // 가장 상단에 `//${fileName}` 형식의 주석을 추가한 형태로 텍스르를 복사한다.
+  const disposable = vscode.commands.registerCommand("YOCO.copyTextWithFilePath", () => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand("YOCO.helloWorld", () => {
-    // The code you place here will be executed every time your command is executed
-    // Display a message box to the user
-    vscode.window.showInformationMessage("Hello World from YOCO!");
+    const document = editor.document;
+
+    const selection = editor.selection;
+    const filePath = document.uri.path;
+    const text = document.getText(selection);
+
+    const comment = `//${filePath}`;
+
+    vscode.env.clipboard.writeText(comment + text);
   });
 
   context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
