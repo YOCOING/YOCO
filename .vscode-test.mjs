92574c1526da99119d13090f4752ebebbe28b84a
givvemee <givvemeee@gmail.com>
2024-04-30 15:08:06 +0900

    [test] test running environment

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,5 +1,10 @@
-import { defineConfig } from '@vscode/test-cli';
+import { defineConfig } from "@vscode/test-cli";
 
 export default defineConfig({
-	files: 'out/test/**/*.test.js',
+  label: "sample-test",
+  files: ["out/test/**/*.test.js", "src/test/**/*.test.js"],
+  mocha: {
+    ui: "tdd",
+    timeout: 20000,
+  },
 });
